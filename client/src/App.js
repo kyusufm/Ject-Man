85ea0eaa80f850b0bb9cb930a7792609e277d4f9
Yusuf K. Muhammad <kyusufm@gmail.com>
2023-01-03 09:06:56 +0700

    add Client Modal featue

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,45 @@
 import './App.css';
+import Header from './components/Header';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import Clients from './components/Clients';
+import AddClientModal from './components/AddClientModal';
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8081/graphql',
+  cache,
+});
 
 function App() {
   return (
+    <>
+    <ApolloProvider client={client}>
+    <Header />
     <div className="container">
-      <h1>hi mom</h1>
+      <AddClientModal />
+      <Clients />
     </div>
+    </ApolloProvider>
+    </>
   );
 }
 
